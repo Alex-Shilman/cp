f4bf14ab0e75d11bf90ae133df2ec34914584a68
Alex Shilman <ashilman999@gmail.com>
2017-03-08 17:35:54 -0500

    added margin to avatar user in nav

--- shared/components/Nav/index.js
+++ shared/components/Nav/index.js
@@ -17,12 +17,12 @@ export default class Navigation extends Component{
         debugger;
         const Person = ({title}) => (
             (user && user.profile)
-                  ?   <span>
-                        <img src={user.profile.picture} style={{width: '20%', borderRadius: '100%'}}/>
+                  ?   <span className="cp-Nav_User">
+                        <img className="cp-Nav_User-Avatar" src={user.profile.picture} />
                         {user.profile.name}
                       </span>
                   :   <span>
-                        <svg className="person" style={{width:22, marginRight:10}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <svg className="cp-Nav_User-Person" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <circle className="colorable svg-white-default" stroke="currentColor" fill="none" cx="12" cy="10" r="3.5"/>
                             <path className="colorable svg-white-default" stroke="currentColor" fill="none" d="M12,14c-4,0-7.21,2.67-7.47,6.05a11,11,0,0,0,14.95,0C19.21,16.67,16,14,12,14Z"/>
                             <path class="colorable svg-white-default" stroke="currentColor" fill="none" d="M23,12A11,11,0,1,0,4.53,20.05C4.79,16.67,8,14,12,14s7.21,2.67,7.47,6.05A11,11,0,0,0,23,12Z"/>
